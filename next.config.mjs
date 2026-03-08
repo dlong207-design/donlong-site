874a72a41d2e18f877f62f31fb72811c5b6c6a89
Julie Long <jdflip8@gmail.com>
2026-03-08 15:24:08 -0600

    Initial site build

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+import createMDX from '@next/mdx'
+import remarkFrontmatter from 'remark-frontmatter'
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkFrontmatter],
+    rehypePlugins: [],
+  },
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+}
+
+export default withMDX(nextConfig)
